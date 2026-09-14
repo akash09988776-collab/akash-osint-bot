a53d2a7b67b1da12dca3dc6f84f8c61e62fcee17
akash09988776-collab <akash09988776-collab@users.noreply.github.com>
2026-09-14 22:31:17 +0530

    Add menu commands, Admin Delegation, fix QueryScope, update My Account

--- bot.py
+++ bot.py
@@ -22,12 +22,12 @@ CHANNELS = [
 ]
 
 # ---------- API URLs ----------
-API_NUMBER = "https://travelers-creature-sarah-rogers.trycloudflare.com/search?q={}"
+API_NUMBER = "https://redxapipanel.vercel.app/api/v1/info?service=numinfo&key=numdemo&query=9006640786={}"
 API_IFSC = "https://vercei-kappa.vercel.app/ifsc?code={}"
 API_PINCODE = "https://nitin-apis-update-birthday-spacial.vercel.app/api?type=pincode&search={}"
 API_WEATHER = "https://nitin-wather-check-api.vercel.app/api?type=weather&search={}"
 API_EMAIL = "https://travelers-creature-sarah-rogers.trycloudflare.com/search?q={}"
-API_AADHAR = "https://travelers-creature-sarah-rogers.trycloudflare.com/search?q={}"
+API_AADHAR = "https://redxapipanel.vercel.app/api/v1/info?service=aadharinfo&key=aadhardemo&query={}"
 API_IP = "https://talks-chain-restrictions-statistics.trycloudflare.com/search?query={}"
 API_PAN = "https://counted-developing-parade-man.trycloudflare.com/pan-info?pan={}"
 API_TG_TO_NUM = "https://tg2num-botadminshere.vercel.app/?id={}"
